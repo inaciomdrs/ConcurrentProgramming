e28c978be55b096da651f531b835c8d2cb1aaae6
Inácio Gomes Medeiros <boyinacio@A303-08L.imd.local>
2016-09-19 21:50:46 -0300

    gives more test cases for producer_consumer problem

--- src/br/ufrn/imd/producer_consumer/Consumer.java
+++ src/br/ufrn/imd/producer_consumer/Consumer.java
@@ -3,18 +3,25 @@ package br.ufrn.imd.producer_consumer;
 public class Consumer extends Thread {
 	private CubbyHole cubbyHole;
 	private int number;
+	private boolean consumed;
 
 	public Consumer(CubbyHole cubbyHole, int number) {
 		super();
 		this.cubbyHole = cubbyHole;
 		this.number = number;
+		this.consumed = false;
 	}
 	
 	public void run(){
 		int value;
 		for (int i = 0; i < 10; i++) {
-			value = cubbyHole.get();
-			System.out.println("Consumer #" + this.number + " got: " + value);
+			if(!consumed){
+				value = cubbyHole.get();
+				System.out.println("Consumer #" + this.number + " got: " + value);
+				consumed = true;
+			} else {
+				consumed = false;
+			}
 		}
 	}
 }
--- src/br/ufrn/imd/producer_consumer/Main.java
+++ src/br/ufrn/imd/producer_consumer/Main.java
@@ -5,14 +5,18 @@ public class Main {
 	public static void main(String[] args) {
 		CubbyHole resource = new CubbyHole();
 		
-		Producer producer = new Producer(resource, 1);
+		Producer producerA = new Producer(resource, 1);
+		Producer producerB = new Producer(resource, 2);
 		
 		Consumer consumerA = new Consumer(resource, 1);
 		Consumer consumerB = new Consumer(resource, 2);
+		Consumer consumerC = new Consumer(resource, 3);
 		
-		producer.start();
+		producerA.start();
+		producerB.start();
 		consumerA.start();
 		consumerB.start();
+		consumerC.start();
 
 	}
 
